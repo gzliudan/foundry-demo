802ea5a04b01f9f567cad83d931dedf7828f1600
Daniel Liu <liudaniel@qq.com>
2023-03-11 19:49:50 +0800

    update test code

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -8,17 +8,31 @@ contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        counter = new Counter(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_SetUpState() public {
+        assertEq(counter.counter(), 0, "counter should be 0 after setup");
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testCount() public {
+        uint256 result = counter.count();
+        assertEq(result, 1, "function count() should return 1");
+    }
+
+    function testCountAsOwner() public {
+        counter.count();
+        assertEq(counter.counter(), 1, "counter should be 1 after call count()");
+    }
+
+    function testFailCountAsNotOwner() public {
+        vm.prank(address(0));
+        counter.count();
+    }
+
+    function test_RevertWhen_CountNotOwner() public {
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(address(0));
+        counter.count();
     }
 }
