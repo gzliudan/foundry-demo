88a56ffe642ac06209e0a3b48a080d3b6930bb98
Daniel Liu <liudaniel@qq.com>
2023-03-11 19:49:26 +0800

    update contract code

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+error Unauthorized();
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Counter {
+    uint256 public counter;
+    address public owner;
+
+    constructor(uint256 value) {
+        counter = value;
+        owner = msg.sender;
     }
 
-    function increment() public {
-        number++;
+    function count() public returns (uint256) {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
+
+        uint256 result = counter + 1;
+        counter = result;
+
+        return result;
     }
 }
