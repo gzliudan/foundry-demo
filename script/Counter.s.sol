3d1ef1916c59041b39c96e30dde74e58225083de
Daniel Liu <liudaniel@qq.com>
2023-03-11 19:50:10 +0800

    update deploy script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,34 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    address internal deployer;
+    string internal mnemonic;
+
+    function saveContract(string memory network, string memory name, address addr) public {
+        string memory filename = string.concat(string.concat("deploy/", network), ".json");
+
+        string memory childObj = "child";
+        vm.serializeString(childObj, "name", name);
+        string memory childJson = vm.serializeAddress(childObj, "address", addr);
+
+        string memory rootObj = "root";
+        vm.serializeString(rootObj, "network", network);
+        string memory rootJson = vm.serializeString(rootObj, name, childJson);
+
+        vm.writeJson(rootJson, filename);
+    }
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter c = new Counter(0);
+        console.log("Counter deployed on %s", address(c));
+        vm.stopBroadcast();
+
+        saveContract("goerli", "Counter", address(c));
     }
 }
